8bd6a2ffea84998453570f715602a32f21cee86f
Rahul Kumar <rahul@api.awasaf.com>
2025-12-16 07:18:55 +0000

    Add Docker container build

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD pipeline is working!"
+    return "CI/CD with Docker is working!"
 
 if __name__ == "__main__":
     host = os.getenv("FLASK_HOST", "127.0.0.1")
     app.run(host=host, port=5000)
-
