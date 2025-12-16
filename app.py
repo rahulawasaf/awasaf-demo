3060d09f0f5b2c0b105d354568a57a9db0e55cc5
Rahul Kumar <rahul@api.awasaf.com>
2025-12-16 01:26:10 +0000

    Fixing vulnerability

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD is working"
-
+    return "CI/CD pipeline is working!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = os.getenv("FLASK_HOST", "127.0.0.1")
+    app.run(host=host, port=5000)
+
